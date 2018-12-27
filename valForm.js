6fffa6f9f7abd84653a080b818c2e8110bb114d0
Artsiom Praneuski <artem.pronevskiy@gmail.com>
2018-12-27 14:16:10 +0300

    Update valForm.js

--- valForm.js
+++ valForm.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function validation_form(forma){//валидация формы
+function validation_form(forma){  //валидация формы
 
     try {
         var inputs = forma.elements;
@@ -12,143 +12,136 @@ function validation_form(forma){//валидация формы
             }
         }
 
-
         var check = arr.filter(validate_input);
-        if (arr.length == check.length) {      //сравниваем массив со всеми input из массива с input которые прошли проверку
+        if (arr.length == check.length) {  //сравниваем массив со всеми input из массива с input которые прошли проверку
             return true;
         } else {
             return false;
         }
-
-
         return true;
     }
-    catch ( Ex )
+    catch (Ex)
     {
         return false;
     }
-
 }
 
-function validate_input(element){         //валидация элемента
+function validate_input(element){  //валидация элемента
 
-    var event=element;
-    var checker=true;                  // проверка на валидность
+    var event = element;
+    var checker = true;  // проверка на валидность
 
-    if(element.target) {
-        element=element.target;
+    if (element.target) {
+        element = element.target;
     }
 
-    if(element.type!="radio"){
-        if(element.value.length!=0){             //проверка чтобы поле было заполнено
-            element.style.border="1px solid #ccc";
-            var remove=element.parentNode.parentNode.getElementsByTagName("span");
-            if(remove.length>=1){
+    if (element.type != "radio"){
+        if (element.value.length != 0) {  //проверка чтобы поле было заполнено
+            element.style.border = "1px solid #ccc";
+
+            var remove = element.parentNode.parentNode.getElementsByTagName("span");
+            if (remove.length >= 1){
                 element.parentNode.parentNode.removeChild(remove[0]);
             }
 
-            switch (element.type){
+            switch (element.type) {
                 case "text":
-                    if(!/[a-zA-Zа-яА-Я']/.test(element.value)&&element.name!='startdate'&&element.name!='persons'){   //проверка на буквы
+                    if (!/[a-zA-Zа-яА-Я']/.test(element.value) && element.name != 'startdate' && element.name != 'persons'){  //проверка на буквы
                         element.scrollIntoView();
-                        element.style.border="1px solid red";
-                        if(event.type!="blur"){
+                        element.style.border = "1px solid red";
+                        if(event.type != "blur"){
                             element.focus();
                         }
                         show_error("Поле должно содержать буквы");
-
                     }
-
                     break;
+
                 case "email":
-                    if(!/.+@.+\..+/i.test(element.value)) {       //проверка на email ('@','.')
+                    if (!/.+@.+\..+/i.test(element.value)) {  //проверка на email ('@','.')
                         element.scrollIntoView();
-                        element.style.border="1px solid red";
-                        if(event.type!="blur"){
+                        element.style.border = "1px solid red";
+                        if(event.type != "blur"){
                             element.focus();
                         }
                         show_error("Укажите email");
                     }
-                    break;
+                break;
             }
-        }
-        else {
+        } else {
             element.scrollIntoView();
-            element.style.border="1px solid red";
-            if(event.type!="blur"){
+
+            element.style.border = "1px solid red";
+            if(event.type != "blur"){
                 element.focus();
             }
             show_error("Заполните поле");
-
-
         }
-    } else {         // если радиокнопки
+    } else {  // если радиокнопки
 
-        if(!check_radio()){
+        if (!check_radio()){
             element.scrollIntoView();
             show_error("Выберите");
-        }else {
-            var remove=element.parentNode.parentNode.getElementsByTagName("span");
-            if(remove.length>=1){
+        } else {
+            var remove = element.parentNode.parentNode.getElementsByTagName("span");
+            if (remove.length >= 1){
                 element.parentNode.parentNode.removeChild(remove[0]);
             }
         }
-
     }
 
-    function show_error(message) {          //вывод ошибки
+    function show_error(message) { //вывод ошибки
 
-        if(element.parentNode.parentNode.getElementsByTagName("span").length<1){
-            var NewSpanElement=document.createElement("span");
-            var NewTextElement=document.createTextNode(message);
-            NewSpanElement.style.color="red";
-            NewSpanElement.style.marginLeft="1em";
+        if(element.parentNode.parentNode.getElementsByTagName("span").length < 1){
+            var NewSpanElement = document.createElement("span");
+            var NewTextElement = document.createTextNode(message);
+            NewSpanElement.style.color = "red";
+            NewSpanElement.style.marginLeft = "1em";
             NewSpanElement.appendChild(NewTextElement);
             element.parentNode.parentNode.appendChild(NewSpanElement);
         }
-        checker=false;         // если ошибка то checker=false
+        checker = false;  // если ошибка то checker=false
     }
 
-    if(checker){
+    if (checker){
         return true;
     } else {
         return false;
     }
 }
-function check_radio() {         //проверка на заполниность радиокнопок
-    var mass= document.querySelectorAll("[type=radio]");
-    var flag=false;
+function check_radio() {  //проверка на заполниность радиокнопок
+    var mass = document.querySelectorAll("[type=radio]");
+    var flag = false;
 
-    for(var i=0;i<mass.length;i++){
-        if(mass[i].checked){
-            flag=true;
+    for (var i = 0; i < mass.length; i++){
+        if (mass[i].checked){
+            flag = true;
         }
     }
 
-    if(flag){
+    if (flag){
         return true;
     } else {
         return false;
     }
-
 }
 
-function submit_form(EO) {           // отправка формы
+function submit_form(EO) {  // отправка формы
     EO=EO||window.event;
-    var forma=EO.target.parentNode.parentNode;
+    var forma = EO.target.parentNode.parentNode;
     EO.preventDefault();
-    if(validation_form(forma)){
+
+    if (validation_form(forma)){
         forma.submit();
     }
-
 }
 
-for (var i=0;i<formSite.elements.length;i++){               //добавляем обработчики
-    if(formSite.elements[i].type=="radio"){
-        formSite.elements[i].onclick=validate_input;
+for (var i = 0; i < formSite.elements.length; i++){  //добавляем обработчики
+    if (formSite.elements[i].type == "radio"){
+        formSite.elements[i].onclick = validate_input;
     } else {
-        formSite.elements[i].onblur=validate_input;
+        formSite.elements[i].onblur = validate_input;
     }
 }
-var SubmitButton=formSite.querySelector("[type=submit]");
+
+var SubmitButton = formSite.querySelector("[type=submit]");
 SubmitButton.addEventListener('click',submit_form,false);
